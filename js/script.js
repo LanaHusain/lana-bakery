a891968518bc7a3896f5e2077bc8ed94e840ef64
amin mahfudi <maulana.hia2@gmail.com>
2024-09-12 09:32:23 +0700

    menambahkan style guide, menambahkan services, menambahkan layanan

--- js/script.js
+++ js/script.js
@@ -2,8 +2,25 @@ const nav = document.querySelector("nav");
 
 document.addEventListener("scroll", () => {
   if (window.scrollY > 0) {
-    nav.setAttribute("class", "scrolled");
+    nav.classList.add("scrolled");
   } else {
-    nav.removeAttribute("class");
+    nav.classList.remove("scrolled");
   }
+});
+
+// Sidebar
+const navList = document.querySelector(".nav-list");
+const hamburger = document.querySelector(".hamburger");
+const navItem = document.querySelectorAll(".nav-list li a");
+
+hamburger.addEventListener("click", () => {
+  navList.classList.toggle("active");
+  hamburger.classList.toggle("active");
+});
+
+navItem.forEach((n) => {
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navList.classList.remove("active");
+  });
 });
